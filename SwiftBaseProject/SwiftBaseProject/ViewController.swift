54a94d6d4ee500681c85ab3327ef8b077e2fc369
zhanshi.wang <2004992@qq.com>
2022-12-01 12:17:13 +0800

    add file

---
+++ SwiftBaseProject/SwiftBaseProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftBaseProject
+//
+//  Created by ZhanshiWang on 2022/12/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
